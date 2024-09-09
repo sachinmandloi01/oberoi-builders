dabbfb0183ffcc47f82b24ff0a1aff79b3591d1a
sourabh-chouhan73 <chouhansourabh2019@gmail.com>
2024-09-09 22:18:53 +0530

    work on real layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.className}>
+      <head>
+        
+      </head>
       <body className={"bg-black text-white font-san"}>{children}</body>
     </html>
   );
