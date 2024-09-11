1fb8933e6c493eed86c69d7e56faf5fc8fc2db3e
Sachin <sachinmandloi@sirfstudy.com>
2024-09-11 16:32:42 +0530

    vide autoplay issue

--- src/app/page.js
+++ src/app/page.js
@@ -88,7 +88,7 @@ export default function Home() {
         }
       });
     };
-  }, []);
+  }, [videos]);
 
   useEffect(() => {
     if (videos.length > 0) {
@@ -120,154 +120,171 @@ export default function Home() {
   return (
     <>
       <div className="app">
-  {isWelcomeModalOpen && (
-    <div className="welcome-modal">
-      <div className="welcome-content">
-        <h2>Welcome to Our Property</h2>
-        <button onClick={closeWelcomeModal}>Enter</button>
-      </div>
-    </div>
-  )}
-  {isModalOpen && <SearchModal onClose={closeModal} />}
-  <header>
-    <nav className="navbar">
-      <div className="container">
-        <div className="location-info">
-          <img src="/carbon_location.png" width={22} height={22} />
-          <h4>Vijay Nagar</h4><i class="fa fa-angle-down" aria-hidden="true"></i>
-          <p>Indore</p>
-          
-        </div>
-        <div className="nav-links">
-          <ul className="nav-group">
-            <li className="nav-item">
-              <a href="#">
-                <img src={"/bx_bookmark-w.png"} height={30} width={30} />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#">
-                <img
-                  onClick={openModal}
-                  src={"/iconamoon_search.png"}
-                  height={31}
-                  width={31}
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="">
-                <img
-                  src={"/solar_point-on-map-bold.png"}
-                  height={30}
-                  width={31}
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="">
-                <i className="far fa-heart"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-
-  <main>
-    <div className="container">
-      <div className="col-9">
-        {videos?.map((item, index) => (
-          <div className="card reel-card" key={index}>
-            <div
-              className="top"
-              onClick={() => router.push(`/details/${item?._id}`)}
-            >
-              <div className="userDetails">
-                <div className="profilepic">
-                  <div className="profile_img">
-                    <div className="image">
+        {isWelcomeModalOpen && (
+          <div className="welcome-modal">
+            <div className="welcome-content">
+              <h2>Welcome to Our Property</h2>
+              <button onClick={closeWelcomeModal}>Enter</button>
+            </div>
+          </div>
+        )}
+        {isModalOpen && <SearchModal onClose={closeModal} />}
+        <header>
+          <nav className="navbar">
+            <div className="container">
+              <div className="location-info">
+                <img src="/carbon_location.png" width={22} height={22} />
+                <h4>Vijay Nagar</h4>
+                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                <p>Indore</p>
+              </div>
+              <div className="nav-links">
+                <ul className="nav-group">
+                  <li className="nav-item">
+                    <a href="#">
+                      <img src={"/bx_bookmark-w.png"} height={30} width={30} />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#">
                       <img
-                        src="https://media.geeksforgeeks.org/wp-content/uploads/20220609093241/g3-200x200.png"
-                        alt="img9"
+                        onClick={openModal}
+                        src={"/iconamoon_search.png"}
+                        height={31}
+                        width={31}
                       />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="">
+                      <img
+                        src={"/solar_point-on-map-bold.png"}
+                        height={30}
+                        width={31}
+                      />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="">
+                      <i className="far fa-heart"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <main>
+          <div className="container">
+            <div className="col-9">
+              {videos?.map((item, index) => (
+                <div className="card reel-card" key={index}>
+                  <div
+                    className="top"
+                    onClick={() => router.push(`/details/${item?._id}`)}
+                  >
+                    <div className="userDetails">
+                      <div className="profilepic">
+                        <div className="profile_img">
+                          <div className="image">
+                            <img
+                              src="https://media.geeksforgeeks.org/wp-content/uploads/20220609093241/g3-200x200.png"
+                              alt="img9"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <h3>
+                        {item?.contactName}
+                        <br />
+                        <span>{item?.location}</span>
+                      </h3>
+                      <div className="builder-location-text">
+                        <img
+                          src={"/solar_point-on-map-bold.png"}
+                          height={30}
+                          width={31}
+                        />
+                        <span className="distance-info">6Km away</span>
+                      </div>
+                    </div>
+                    <div>
+                      <span className="dot">
+                        <i className="fas fa-ellipsis-h"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="imgBx">
+                    <div className="video-container">
+                      <video
+                        src={`${process.env.NEXT_PUBLIC_API_URL}/uploads/videos/${item.videoFiles[0]}`}
+                        autoPlay
+                        loop
+                        playsInline
+                        className="video-player"
+                        controls={false}
+                        muted={isMuted}
+                        ref={(el) => (videoRefs.current[index] = el)}
+                        onEnded={() => handleVideoEnded(index)}
+                      ></video>
+                      {isMuted && (
+                        <div className="unmute-button" onClick={handleUnmute}>
+                          <img
+                            src="/1234.png"
+                            alt="test"
+                            width={25}
+                            height={20}
+                          />
+                        </div>
+                      )}
+                      {!isMuted && (
+                        <div className="unmute-button" onClick={handleMute}>
+                          <img
+                            src="/Speaker_Icon.svg.png"
+                            alt="test"
+                            width={25}
+                            height={20}
+                          />
+                        </div>
+                      )}
+                    </div>
+
+                    <div className="bottom">
+                      <div className="actionBtns">
+                        <img src={"/bx_bookmark.png"} height={30} width={30} />
+                        <a
+                          href="https://wa.me/9669563039?text=Hello%2C%20I%20would%20like%20to%20know%20more%20about%20your%20services."
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="btn"
+                        >
+                          <img src={"/whatsapp.png"} height={28} width={28} />
+                        </a>
+                        <a href="tel:+1234567890" className="btn">
+                          <img
+                            src={"/basil_phone-outline-b.png"}
+                            height={34}
+                            width={34}
+                          />
+                        </a>
+                        <img
+                          src={"/bitcoin-icons_share-outline.png"}
+                          height={33}
+                          width={33}
+                        />
+                      </div>
+                      <div>
+                        <button className="reviews-button">Reviews</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <h3>
-                  {item?.contactName}
-                  <br />
-                  <span>{item?.location}</span>
-                </h3>
-                <div className="builder-location-text">
-                  <img
-                    src={"/solar_point-on-map-bold.png"}
-                    height={30}
-                    width={31}
-                  />
-                  <span className="distance-info">6Km away</span>
-                </div>
-              </div>
-              <div>
-                <span className="dot">
-                  <i className="fas fa-ellipsis-h"></i>
-                </span>
-              </div>
+              ))}
             </div>
-            <div className="imgBx">
-              <div className="video-container">
-                <video
-                  src={`${process.env.NEXT_PUBLIC_API_URL}/uploads/videos/${item.videoFiles[0]}`}
-                  autoPlay
-                  loop
-                  playsInline
-                  className="video-player"
-                  controls={false}
-                  muted={isMuted}
-                  ref={(el) => (videoRefs.current[index] = el)}
-                  onEnded={() => handleVideoEnded(index)}
-                ></video>
-                {isMuted && (
-                  <div className="unmute-button" onClick={handleUnmute}>
-                    <img src="/1234.png" alt="test" width={25} height={20} />
-                  </div>
-                )}
-                {!isMuted && (
-                  <div className="unmute-button" onClick={handleMute}>
-                    <img src="/Speaker_Icon.svg.png" alt="test" width={25} height={20} />
-                  </div>
-                )}
-              </div>
-
-
-              <div className="bottom">
-              <div className="actionBtns">
-                <img src={"/bx_bookmark.png"} height={30} width={30} />
-                <a href="https://wa.me/9669563039?text=Hello%2C%20I%20would%20like%20to%20know%20more%20about%20your%20services." target="_blank" rel="noopener noreferrer" className="btn">
-                 
-                 
-                  <img src={"/whatsapp.png"} height={28} width={28} />
-                </a>
-                <a href="tel:+1234567890" className="btn">
-                  <img src={"/basil_phone-outline-b.png"} height={34} width={34} />
-                </a>
-                <img src={"/bitcoin-icons_share-outline.png"} height={33} width={33} />
-
-                </div>
-                <div>
-                <button className="reviews-button">Reviews</button>
-              </div>
-            </div>
-            </div>
-            
           </div>
-        ))}
+        </main>
       </div>
-    </div>
-  </main>
-</div>
-
     </>
   );
 }
